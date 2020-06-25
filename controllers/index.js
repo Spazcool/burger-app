c08da4be8e46458462a03aa76353d470577f7fa2
Spazcool <douglas.wrightiii@gmail.com>
2020-06-24 21:11:16 -0400

    pushing remote for assistance

--- controllers/index.js
+++ controllers/index.js
@@ -1,14 +1,8 @@
-// this is the entry point to the routing tree that we will be creating for our api
-
-// the first step is to instantiate a new instance of express router as below.
-// express router behaves similarly to an express app that you would create in the server file, however it is generic and can be modularized/exported
 const router = require('express').Router();
+const apiRoutes = require('./apiRoutes.js');
+const htmlRoutes = require('./htmlRoutes');
 
-
-const apiRoutes = require('./burgers_controller.js')
-router.use('/api', apiRoutes)
-
-// const htmlRoutes = require('./htmlRoutes')
-// router.use('/',htmlRoutes)
+router.use('/api', apiRoutes);
+router.use('/', htmlRoutes);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 const express = require('express')
 const app = express();
+const path = require("path");
+const exphbs = require("express-handlebars");
 require('dotenv').config();
 
 const PORT = process.env.PORT || 8080;
-const routes = require('./controllers');
+const routes = require('./controller/index.js');
 
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("public"));
+// app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
+
 app.use('/', routes);
 
 app.listen(PORT, () => console.log(`server listening on port ${PORT}`));
-
