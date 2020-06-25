2c86637c1b0a5bd35e10153e4f366203e41b1fee
Spazcool <douglas.wrightiii@gmail.com>
2020-06-25 10:21:13 -0400

    utilizing path for dirname

--- server.js
+++ server.js
@@ -11,8 +11,8 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("public"));
-// router.use(express.static(path.join(__dirname, "public")));
+// app.use(express.static("public"));
+router.use(express.static(path.join(__dirname, "public")));
 
 app.use('/', routes);
 
